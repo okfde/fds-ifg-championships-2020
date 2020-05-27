2a4d140bb1b10a288d4bcd358bfb8d3ab4fd3fdb
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2020-05-27 15:49:43 +0200

    tournament-visual WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,3 @@
-function createElement (tagname, opts) {
-  var el = document.createElement(tagname)
-
-  // add classes
-  if (Array.isArray(opts.classes)) {
-    var classIdx = opts.classes.length
-    while (classIdx--) {
-      el.classList.add(opts.classes[classIdx])
-    }
-  }
-
-  // inner text
-  if (typeof opts.text === 'string') {
-    el.textContent = opts.text
-  }
-
-  // inner html
-  if (typeof opts.html === 'string') {
-    el.innerHTML = opts.html
-  }
-
-  // attributes
-  if (opts.attrs) {
-    var keys = Object.keys(opts.attrs)
-    for (var i = 0, l = keys.length; i < l; i++) {
-      var k = keys[i]
-      el.setAttribute(k, opts.attrs[k])
-    }
-  }
-
-  // return a string
-  if (opts.returnString && opts.returnString === true) {
-    if (typeof el.outerHTML !== 'undefined') {
-      return el.outerHTML
-    } else {
-      // polyfill
-      var div = document.createElement('div')
-      div.appendChild(el.cloneNode(true))
-      return div.innerHTML
-    }
-  }
-
-  return el
-}
-
 const teams = {
   bw: {
     captain: 'Winfried Kretschmann',
@@ -172,100 +127,122 @@ const rounds = [
       },
     ]
   },
-  // {
-  //   title: 'Runde 2',
-  //   matches: [
-
-  //   ]
-  // },
-  // {
-  //   title: 'Runde Halbfinale',
-  //   matches: [
-
-  //   ]
-  // },
-  // {
-  //   title: 'Finale',
-  //   matches: [
-
-  //   ]
-  // },
+  {
+    title: 'Runde 2',
+    matches: [
+      {
+        team1: 'sh',
+        team2: 'st',
+        winner: 'st'
+      },
+      {
+        team1: 'hb',
+        team2: 'mv',
+        winner: 'mv'
+      },
+      {
+        team1: 'rp',
+        team2: 'he',
+        winner: 'he'
+      },
+      {
+        team1: 'be',
+        team2: 'bb',
+        winner: 'bb'
+      },
+    ]
+  },
+  {
+    title: 'Runde Halbfinale',
+    matches: [
+      {
+        team1: 'sh',
+        team2: 'st',
+        winner: 'st'
+      },
+      {
+        team1: 'hb',
+        team2: 'mv',
+        winner: 'mv'
+      },
+    ]
+  },
+  {
+    title: 'Finale',
+    matches: [
+      {
+        team1: 'sh',
+        team2: 'st',
+        winner: 'st'
+      },
+    ]
+  },
 ]
 
 function init () {
   const rootContainer = document.getElementById('tournament-visual')
-  const outerContainer = createElement('div', {
-    classes: ['tournament-bracket', 'tournament-bracket--rounded']
-  })
-  debugger
-
-  for (let i = 0, l = rounds.length; i < l; i++) {
-    const round = rounds[i]
-    const roundContainer = createElement('div', {
-      classes: ['tournament-bracket__round']
-    })
-    const roundTitle = createElement('h3', {
-      classes: ['tournament-bracket__round-title'],
-      text: round.title
-    })
-    const roundMatchesList = createElement('ul', {
-      classes: ['tournament-bracket__list'],
-      text: round.title,
-      html: createMatchesListHtml(round.matches)
-    })
-
-    roundContainer.appendChild(roundTitle)
-    roundContainer.appendChild(roundMatchesList)
-    outerContainer.appendChild(roundContainer)
-  }
-
-  rootContainer.appendChild(outerContainer)
+  rootContainer.innerHTML = `
+    <div class="tournament-bracket tournament-bracket--rounded">
+      ${createRounds(rounds)}
+    </div>
+    `
 }
 
-function createMatchesListHtml (matchesArr) {
+function createRounds (rounds) {
   let result = ''
-
-  for (let i = 0, l = matchesArr.length; i < l; i++) {
-    const match = matchesArr[i]
-    result += createElement('li', {
-      classes: ['tournament-bracket__item'],
-      html: createMatchHtml(match),
-      returnString: true
-    })
+  for (let i = 0, l = rounds.length; i < l; i++) {
+    result += createRound(rounds[i])
   }
-
   return result
 }
 
-function createMatchHtml (match) {
+function createRound (round) {
+  return `
+  <div class="tournament-bracket__round">
+    <h3 class="tournament-bracket__round-title">${round.title}</h3>
+    <ul class="tournament-bracket__list">
+      ${createMatches(round.matches)}
+    </ul>
+  </div>
+  `
+}
+
+function createMatches (matchesArr) {
+  let result = ''
+  for (let i = 0, l = matchesArr.length; i < l; i++) {
+    result += createMatch(matchesArr[i])
+  }
+  return result
+}
+
+function createMatch (match) {
   const team1 = teams[match.team1]
   const team2 = teams[match.team2]
 
   return `
-  <div class="tournament-bracket__match" tabindex="0">
-    <table class="tournament-bracket__table">
-      <tbody class="tournament-bracket__content">
-        <tr class="tournament-bracket__team">
-          <td class="tournament-bracket__image">
-            <img src="img/${team1.img}">
-          </td>
-          <td class="tournament-bracket__name">
-            ${team1.captain}&nbsp;
-            <span class="tournament-bracket__token" title="${team1.state}">${match.team1}</span>
-          </td>
-        </tr>
-        <tr class="tournament-bracket__team">
-          <td class="tournament-bracket__image">
-            <img src="img/${team2.img}">
-          </td>
-          <td class="tournament-bracket__name">
-            ${team2.captain}&nbsp;
-            <span class="tournament-bracket__token" title="${team2.state}">${match.team2}</span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <li class="tournament-bracket__item">
+    <div class="tournament-bracket__match" tabindex="0">
+      <table class="tournament-bracket__table">
+        <tbody class="tournament-bracket__content">
+          ${createTeam(match.team1, team1)}
+          ${createTeam(match.team2, team2)}
+        </tbody>
+      </table>
+    </div>
+  </li>
+  `
+}
+
+function createTeam (key, props) {
+  return `
+  <tr class="tournament-bracket__team">
+    <td class="tournament-bracket__image">
+      <img src="img/${props.img}">
+    </td>
+    <td class="tournament-bracket__name">
+      ${props.captain}&nbsp;<span class="tournament-bracket__token" title="${props.state}">(${key})</span>
+    </td>
+  </tr>
   `
 }
 
