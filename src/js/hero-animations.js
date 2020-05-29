feead72e6df4a2c0591d66b2e2c20f2a07573502
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2020-05-29 18:38:54 +0200

    hero animations wip

--- src/js/hero-animations.js
+++ src/js/hero-animations.js
@@ -2,8 +2,62 @@
 
   function init () {
     const pathHelperImage = document.getElementById('path-helper')
-    const imagePath = pathHelperImage.src.replace('1x1.png', '')
-    console.warn('imagePath', imagePath)
+    const imgDirPath = pathHelperImage.src.replace('1x1.png', '')
+    renderAnimations(imgDirPath)
+  }
+
+  function renderAnimations (imgDirPath) {
+    const container = document.querySelector('.hero')
+    const containerSize = { w: container.clientWidth, h: container.clientHeight }
+    const teamsArr = Object.values(teams)
+
+    // create, insert and animate images
+    const allImgs = []
+    for (let i = 0, l = teamsArr.length; i < l; i++) {
+      const imgPath = imgDirPath + teamsArr[i].img
+      animateImage(imgPath, container, containerSize)
+    }
+  }
+
+  function renderImage (imgPath) {
+    const el = document.createElement('img')
+    el.classList.add('floating-head')
+    el.setAttribute('src', imgPath)
+    return el
+  }
+
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function getAnimationTime () {
+    const miliseconds = 15000
+    return miliseconds
+  }
+  
+  function animateImage (imgPath, container, containerSize) {
+    const randomDelay = getRandomArbitrary(0, 20000)
+    setTimeout(() => {
+      const imgEl = renderImage(imgPath)
+      container.appendChild(imgEl)
+      
+      const miliseconds = getAnimationTime()
+      const imgHeight = imgEl.clientHeight
+      const imgWidth = imgEl.clientWidth
+      const minY = imgHeight / 2
+      const maxY = containerSize.h - Math.round(imgHeight * 1.5)
+      const randomY = getRandomArbitrary(minY, maxY)
+      const targetXPos = containerSize.w + imgWidth
+  
+      imgEl.style.top = randomY + 'px'
+      imgEl.style.left = -(imgWidth) + 'px'
+      imgEl.style.transform = `translate(${targetXPos}px, 0)`;
+      imgEl.style.transition = `all ${miliseconds}ms linear`;
+      setTimeout(() => {
+        container.removeChild(imgEl)
+        animateImage(imgPath, container, containerSize)
+      }, miliseconds);      
+    }, randomDelay)
   }
 
   init()
